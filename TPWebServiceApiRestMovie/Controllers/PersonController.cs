aaddb7666c455629de6486d7f982afd993c60787
Ninotter <nino.didier@live.fr>
2023-10-30 10:03:13 +0100

    person CRUD

--- TPWebServiceApiRestMovie/Controllers/PersonController.cs
+++ TPWebServiceApiRestMovie/Controllers/PersonController.cs
@@ -21,9 +21,11 @@ namespace TPWebServiceApiRestMovie.Controllers
         /// <remarks>
         /// Sample request:
         /// 
-        ///     "surname": "Buscemi",
-        ///     "name": "Steve",
-        ///     "birthDate": "1957-12-13T00:00:00.145Z"
+        ///     {
+        ///         "surname": "Buscemi",
+        ///         "name": "Steve",
+        ///         "birthDate": "1957-12-13T00:00:00.145Z"
+        ///     }
         ///     
         /// </remarks>
         /// 
@@ -36,8 +38,8 @@ namespace TPWebServiceApiRestMovie.Controllers
             //Generates a new ID if the user mistakenly specified one
             person.Id = 0;
             //Clears specified movies
-            //person.MoviesPlayed.Clear();
-            //person.MoviesDirected.Clear();
+            person.MoviesPlayed.Clear();
+            person.MoviesDirected.Clear();
             _context.Persons.Add(person);
             _context.SaveChanges();
 
@@ -62,6 +64,66 @@ namespace TPWebServiceApiRestMovie.Controllers
                 return new JsonResult(NotFound());
             }
 
+            return new JsonResult(Ok(result));
+        }
+
+        /// <summary>
+        /// Edits a person
+        /// </summary>
+        /// <remarks>
+        /// Sample request:
+        /// 
+        ///     {
+        ///        "id" : 1,
+        ///         "surname": "Buscemi",
+        ///         "name": "Steve",
+        ///         "birthDate": "1957-12-13T00:00:00.145Z"
+        ///     }
+        ///
+        /// </remarks>
+        [ProducesResponseType(200, Type = typeof(Person))]
+        [ProducesResponseType(404)]
+        [Produces("application/json")]
+        [HttpPost]
+        public JsonResult Edit(Person person)
+        {
+            var personInDb = _context.Persons.Find(person.Id);
+
+            if (personInDb == null)
+            {
+                Response.StatusCode = 404;
+                return new JsonResult(NotFound());
+            }
+
+            _context.Entry(personInDb).CurrentValues.SetValues(person);
+            _context.SaveChanges();
+
+            Response.StatusCode = 200;
+            return new JsonResult(Ok(person));
+        }
+
+
+        /// <summary>
+        /// Deletes a specific person
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpDelete]
+        [ProducesResponseType(200, Type = typeof(Person))]
+        [ProducesResponseType(404)]
+        public JsonResult Delete(int id)
+        {
+            var result = _context.Persons.Find(id);
+
+            if (result == null)
+            {
+                Response.StatusCode = 404;
+                return new JsonResult(NotFound());
+            }
+            _context.Persons.Remove(result);
+            _context.SaveChanges();
+
+            Response.StatusCode = 200;
             return new JsonResult(Ok(result));
         }
     }
